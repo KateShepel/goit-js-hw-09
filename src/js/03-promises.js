2e8b861409820c10bb50da964e0bbbcb5f5a35a3
KateShepel <MariyaKaterina@gmail.com>
2023-05-29 11:09:19 +0300

    Додає ДЗ 9.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
+
+const refs = {
+  delayInput: document.querySelector('input[name="delay"]'),
+  stepInput: document.querySelector('input[name="step"]'),
+  amountInput: document.querySelector('input[name="amount"]'),
+  btn: document.querySelector('button[type="submit"]'),
+};
+
+refs.btn.addEventListener('click', onBtnClick);
+
+function onBtnClick(e) {
+  e.preventDefault();
+
+  for (let i = 0; i < refs.amountInput.value; i += 1) {
+    createPromise(
+      i + 1,
+      Number(refs.delayInput.value) + Number(refs.stepInput.value) * i
+    )
+      .then(result => {
+        Notiflix.Notify.success(result);
+      })
+      .catch(result => {
+        Notiflix.Notify.failure(result);
+      });
   }
 }
